22b42a4d446bae8677ffac9c89e085f574c5a787
brockcp <brockpatterson@gmail.com>
2018-01-03 15:15:56 -0800

    devise view mods

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171229190831) do
+ActiveRecord::Schema.define(version: 20171230174409) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20171229190831) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
