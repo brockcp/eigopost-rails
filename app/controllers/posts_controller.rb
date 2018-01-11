66d3a0167e84e30081af61f82e5a909c4dc07cd5
brockcp <brockpatterson@gmail.com>
2018-01-10 20:59:41 -0800

    moved comment form to post/show(render)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-   
+
     def index
       @posts = Post.all
     end
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
       @post.public = params[:post][:public]
       @post.user = current_user
       if @post.save
-        flash[:notice] = "Saved"
+        flash[:notice] = "Thank you. Your post is now online."
         redirect_to @post
       else
         flash.now[:alert] = "Not Saved"
@@ -27,32 +27,32 @@ class PostsController < ApplicationController
       end
     end
 
-    def edit
-      @post = Post.find(params[:id])
-    end
-
-    def update
-      @post = Post.find(params[:id])
-      @post.name = params[:post][:name]
-      @post.description = params[:post][:description]
-      if @post.save
-        flash[:notice] = "Saved!"
-        redirect_to @post
-      else
-        flash.now[:alert] = "Error!"
-        render :edit
-      end
-    end
-
-    def destroy
-      @post = Post.find(params[:id])
-      if @post.destroy
-        flash[:notice] = "\"#{@post.name}\" was deleted."
-        redirect_to @post
-      else
-        flash.now[:alert] = "Couldnt delete."
-        render :show
-      end
-    end
+    # def edit
+    #   @post = Post.find(params[:id])
+    # end
+    #
+    # def update
+    #   @post = Post.find(params[:id])
+    #   @post.name = params[:post][:name]
+    #   @post.description = params[:post][:description]
+    #   if @post.save
+    #     flash[:notice] = "Saved!"
+    #     redirect_to @post
+    #   else
+    #     flash.now[:alert] = "Error!"
+    #     render :edit
+    #   end
+    # end
+    #
+    # def destroy
+    #   @post = Post.find(params[:id])
+    #   if @post.destroy
+    #     flash[:notice] = "\"#{@post.name}\" was deleted."
+    #     redirect_to @post
+    #   else
+    #     flash.now[:alert] = "Couldnt delete."
+    #     render :show
+    #   end
+    # end
 
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :user
 
+  default_scope { order(created_at: :asc) } #desc
+
   validates :name, length: { minimum: 5 }, presence: true
   validates :description, length: { minimum: 15 }, presence: true
   validates :user, presence: true
