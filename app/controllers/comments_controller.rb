54c4e10749ce2189b51a4db9bcef1d75f50a7c30
brockcp <brockpatterson@gmail.com>
2018-01-13 17:32:05 -0800

    voting working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,6 @@ class CommentsController < ApplicationController
     end
 
     private
-
     def comment_params
       params.require(:comment).permit(:body)
     end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,32 +27,4 @@ class PostsController < ApplicationController
       end
     end
 
-    # def edit
-    #   @post = Post.find(params[:id])
-    # end
-    #
-    # def update
-    #   @post = Post.find(params[:id])
-    #   @post.name = params[:post][:name]
-    #   @post.description = params[:post][:description]
-    #   if @post.save
-    #     flash[:notice] = "Saved!"
-    #     redirect_to @post
-    #   else
-    #     flash.now[:alert] = "Error!"
-    #     render :edit
-    #   end
-    # end
-    #
-    # def destroy
-    #   @post = Post.find(params[:id])
-    #   if @post.destroy
-    #     flash[:notice] = "\"#{@post.name}\" was deleted."
-    #     redirect_to @post
-    #   else
-    #     flash.now[:alert] = "Couldnt delete."
-    #     render :show
-    #   end
-    # end
-
-  end
+end
