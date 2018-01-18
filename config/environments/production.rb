98350eb6c37390d7bd0d9f6c1ddc9b728c5608cf
brockcp <brockpatterson@gmail.com>
2018-01-18 15:31:09 -0800

    sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,7 +103,7 @@ Rails.application.configure do
     authentication: :plain,
     user_name:      ENV['SENDGRID_USERNAME'],
     password:       ENV['SENDGRID_PASSWORD'],
-    domain:         'eigopost.herokuapp.com',
+    domain:         'heroku.com',
     enable_starttls_auto: true
   }
 
