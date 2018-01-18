405114313ed5e670bde382610c328c0a55972ad7
brockcp <brockpatterson@gmail.com>
2018-01-17 20:07:49 -0800

    secret key fun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.secret_key_base = ENV["DEVISE_SECRET_KEY"]
+  #changed secret key
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
