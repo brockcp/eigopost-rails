57fc51ce3dcdcfda3fadec1b1056820d9b3ce622
brockcp <brockpatterson@gmail.com>
2018-01-18 23:14:09 -0800

    dev and prod functioning with postgres

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,8 +60,8 @@ Rails.application.configure do
     address:        'smtp.sendgrid.net',
     port:           '587',
     authentication: :plain,
-    user_name:      ENV['SENDGRID_USERNAME'],
-    password:       ENV['SENDGRID_PASSWORD'],
+    user_name:      ENV['SENDGRID_USERNAME_DEV'],
+    password:       ENV['SENDGRID_PASSWORD_DEV'],
     domain:         'localhost:3000',
     enable_starttls_auto: true
   }
