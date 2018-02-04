a14309c78b078ae73159675365f156abac9db16c
brockcp <brockpatterson@gmail.com>
2018-02-03 20:59:14 -0800

    updating devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,15 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :current_password) }
     end
 
+    layout :layout
+
+  private
+
+  def layout
+    # only turn it off for login pages:
+    # is_a?(Devise::SessionsController) ? false : "application"
+    # or turn layout off for every devise controller:
+     !devise_controller? && "application"
+  end
+
 end
