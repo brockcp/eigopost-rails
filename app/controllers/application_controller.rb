6776913b2ef063fb7a3476d751fd93b18fe410c8
brockcp <brockpatterson@gmail.com>
2018-02-05 10:17:51 -0800

    updating devise logins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,10 +20,8 @@ class ApplicationController < ActionController::Base
   private
 
   def layout
-    # only turn it off for login pages:
-    # is_a?(Devise::SessionsController) ? false : "application"
-    # or turn layout off for every devise controller:
-     !devise_controller? && "application"
+    # is_a?(Devise::SessionsController) ? false : "application" # JUSTLOGIN PAGES
+     !devise_controller? && "application" #ALL DEVISE CONTROLLERS
   end
 
 end
